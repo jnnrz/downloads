64b776f9125aaabac5b08972300f8be5ed336505
Johann Perozo <johann.prz@gmail.com>
2019-05-27 17:41:00 -0400

    Removes most of background script

--- public/app/background.js
+++ public/app/background.js
@@ -1,50 +1,6 @@
 /* global chrome */
 
 function main() {
-  /*chrome.runtime.onInstalled.addListener(function () {
-    chrome.bookmarks.search("Bookmark for Later", function (res) {
-      if (!(res.length > 0)) {
-        // If main folder doesn't exist, create it
-        chrome.bookmarks.create({ parentId: "2", title: "Bookmark for Later" }, function (res) {
-          console.log(res.id);
-          chrome.storage.local.set({ "bfl_folder_id": res.id }, function () {
-            if (chrome.runtime.lastError) {
-              alert("Oops... " + chrome.runtime.lastError.message);
-            }
-          })
-        });
-      } else {
-        chrome.storage.local.set({ "bfl_folder_id": res[0].id });
-      }
-    });
-  });
-
-  // Create context menu
-  chrome.contextMenus.create(
-    {
-      id: "bflcm",
-      title: "Bookmark this site"
-    },
-    function () {
-      if (chrome.runtime.lastError) {
-        console.warn("Oops... " + chrome.runtime.lastError.message);
-      }
-    }
-  )
-
-  chrome.contextMenus.onClicked.addListener(function (info) {
-    if (info.menuItemId === "bflcm") {
-      chrome.tabs.getSelected(function (tab) {
-        chrome.storage.local.get("bfl_folder_id", (data) => {
-          chrome.bookmarks.create({
-            parentId: data.bfl_folder_id,
-            title: tab.title,
-            url: tab.url
-          });
-        });
-      });
-    }
-  });*/
 }
 
 main();
