9ea75d150dcde56f68a98ff034039f87ba9be278
Johann Perozo <johann.prz@gmail.com>
2019-05-26 22:11:36 -0400

    Functionality 80% done

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,64 @@
 /*global chrome*/
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [downloadItems, setDownloadItems] = useState([]);
+
+  // Similar to componentDidMount
+  useEffect(() => {
+    chrome.downloads.search({ exists: true, state: "complete", orderBy: ['-startTime'] }, function (res) {
+      setDownloadItems([...res]);
+    });
+  });
+
+  const openFile = (e, downloadId) => {
+    e.preventDefault();
+    chrome.downloads.open(downloadId);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {downloadItems.length > 0 ? (
+        <ul className="nice-list p-0">
+          {downloadItems.map((item) => {
+            return (
+              <li className="nice-list-item">
+                <table>
+                  <tr>
+                    <th style={{ width: '100px' }}></th>
+                    <th></th>
+                  </tr>
+                  <tr>
+                    <td></td>
+                    <td>
+                      <div className="list-main-content d-flex justify-content-between">
+                        <div>
+                          <h6 className="m-0">
+                            <a href={item.url}
+                              onClick={e => openFile(e, item.id)}>{item.filename.replace(/^.*[\\\/]/, '')}</a>
+                          </h6>
+                          <p className="smallp mb-1">{item.filename}</p>
+                        </div>
+                        <div class="btn-group dropleft btn-menu">
+                          <button type="button btn-sm" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">menu</button>
+                          <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Action</a>
+                          </div>
+                        </div>
+                      </div>
+                      <div>
+                        <p className="smallp mb-1">{item.fileSize / 1000} KB</p>
+                      </div>
+                    </td>
+                  </tr>
+                </table>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (<h3>There are no downloads.</h3>)}
     </div>
   );
 }
